04894c66c38bd202a34c7c1be174e243f9e23cd8
sanghyun <pu51@naver.com>
2023-09-20 02:59:59 +0000

    chap2

---
+++ chap2/prob4.py
@@ -0,0 +1,10 @@
+input("Hi\tWhat's your name? ")
+age = int(input("And how old are you? "))
+weigh = int(input("Okay, last question.\tHow many pounds do you weigh? "))
+print('')
+
+dogYears = age / 7
+
+print("Did you now that you're just", dogYears, "in dog years?")
+print('')
+print("But you're also over 
